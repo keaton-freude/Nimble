8dee7c0493935a3b134b28522a265a4e441434e8
Keaton Freude <freudek90@gmail.com>
2016-03-02 23:10:59 -0800

    Add particles_per_second BMI init

--- NimbleGraphics/NimbleGraphics/src/ParticleSettings.cpp
+++ NimbleGraphics/NimbleGraphics/src/ParticleSettings.cpp
@@ -6,7 +6,7 @@ ParticleSettings::ParticleSettings()
 	duration(12.0f), duration_randomness(3.0f), min_velocity(Vector3(-5.0f, -5.0f, -5.0f)), max_velocity(Vector3(5.0f, 5.0f, 5.0f)),
 	gravity(Vector3(0.0f, 0.0f, 0.0f)), end_velocity(1.0f), min_color(Color(1.0f, 1.0f, 1.0f, 1.0f)), max_color(Color(1.0f, 1.0f, 1.0f, 1.0f)),
 	min_rotate_speed(0.0f), max_rotate_speed(0.0f), start_size(0.0f, 0.0f), end_size(0.0f, 0.0f),
-	blend_state(BLEND_STATE::Alpha)
+	blend_state(BLEND_STATE::Alpha), particles_per_second(0.0f)
 {
 	LOG_INFO("ParticleSettings default c'tor");
 }
